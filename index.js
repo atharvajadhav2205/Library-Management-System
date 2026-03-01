5b47bef7c5a46d3226ebe305184dc463cc5b0ec6
atharvajadhav2205 <atharvaj2205@gmail.com>
2026-03-01 21:38:50 +0530

    modified

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express =  require ("express");
 
+const { users } = require("./data/users.json");
+
 const app = express();
 
 const port = 8081;
@@ -11,6 +13,43 @@ app.get("/",(req,res)=>{
         message :"Welcome to home page :-)"
     })
 })
+/**
+ * Route : /users
+ * Method : GET
+ * Description : Get all users
+ * Access : Public
+ * Parameters : none
+ */
+
+app.get("/users",(req,res)=>{
+    res.status(200).json({
+        success : true,
+        data : users
+    })
+})
+
+/**
+ * Route : /users/:id
+ * Method : GET
+ * Description : Get a specific user by ID
+ * Access : Public
+ * Parameters : none
+ */
+
+app.get("/users/:id",(req,res)=>{
+    const {id} = req.params;
+    const user = users.find((user)=> user.id === id);
+    if(!user){
+        return res.status(404).json({
+            success : false,
+            message : `User with ID ${id} not found`
+        })
+    }
+    res.status(200).json({
+        success : true,
+        data : user
+    })
+})  
 
 app.listen(port,()=>{
     console.log(`Server running on port http://localhost:${port}`);
